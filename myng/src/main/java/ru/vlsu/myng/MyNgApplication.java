beec33db867f79517603121744b1d02337bc4bd1
Meshok54 <mishaoles@gmail.com>
2026-02-11 17:25:56 +0300

    Добавлена структура проекта с помощью Spring Boot

---
+++ myng/src/main/java/ru/vlsu/myng/MyNgApplication.java
@@ -0,0 +1,13 @@
+package ru.vlsu.myng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNgApplication.class, args);
+	}
+
+}
---
+++ myng/src/test/java/ru/vlsu/myng/MyNgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlsu.myng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
